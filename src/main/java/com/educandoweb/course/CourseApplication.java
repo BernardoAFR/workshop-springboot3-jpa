030795bf0c72574c9686d16f6f8298a9e09a6649
Bernardo Rodrigues <bernardofigueiredo387@gmail.com>
2024-07-31 17:54:40 -0300

    Project created

---
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // configura o projeto para ser uma aplicação springBoot
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
